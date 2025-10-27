5fbd7a7ccf7cad9b4f047094346710a476963d39
verbose <ev3rnub@gmail.com>
2025-10-26 17:08:25 -0700

    Cleaned up some unused javascript in index.js.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
+// Chad Verbus
+// CSC102
+// 10/26/2025
 // GET buttons, image and video source via element ID
-const aStartButton = document.getElementById('startBtn');
-const aStopButton = document.getElementById('stopBtn');
-const img = document.getElementById('memeImage');
 const aVid = document.getElementById('video');
 const aVidBtn = document.getElementById('playBtn');
 
@@ -14,36 +13,4 @@ aVidBtn.addEventListener('click', () => {
     aVid.pause();
     aVidBtn.innerHTML = "Play Video"
   }
-});
-
-
-
-// start button functionality
-function disableStartButton() {
-    aStartButton.disabled = true;
-    aStopButton.disabled = false;
-}
-
-// stop button functionality
-function disableStopButton() {
-    aStopButton.disabled = true;
-    aStartButton.disabled = false;
-}
-
-
-/* ---------- PLAY Animation ---------- */
-aStartButton.addEventListener('click', () => {
-  // Remove the class (in case it was already there)
-  img.classList.remove('animate');
-
-  // Force re‑flow – guarantees a fresh start
-  void img.offsetWidth;
-
-  // Add the class → animation runs from the beginning
-  img.classList.add('animate');
-});
-
-/* ---------- STOP Anination ---------- */
-aStopButton.addEventListener('click', () => {
-  img.classList.remove('animate');
 });
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 Course: CSC102
 Author: Chad Verbus
-Date: Oct 19, 2025
+Date: Oct 26, 2025
 
 
 ⚠️ Working Document – This README is a living document that will be updated throughout the semester as new topics, assignments, and resources become available.  
 
 ## 10/19/2025
 * 1. Copied previous assignment, added video with sound. Updated ReadMe.
+## 10/26/2025
+* 1. Added a second video to the main page, Added a music page that plays one song. I also removed some unused css. 
+* 2. Cleaned up some unused javascript in index.js.
